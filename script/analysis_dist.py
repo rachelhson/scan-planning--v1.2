124bcb0238e98c1e4015bb1760e94d903b4865e3
Rachel Hyo Son <52179017+rachelhson@users.noreply.github.com>
2022-04-01 23:42:00 -0400

    Update analysis_dist.py

--- script/analysis_dist.py
+++ script/analysis_dist.py
@@ -1,9 +1,9 @@
-from module2 import downpcd
 import open3d as o3d
-import time
+import time 
 start_time = time.time()
-voxeledpcd = downpcd.voxel_down_sample(voxel_size=0.005)
-#o3d.visualization.draw_geometries([voxeledpcd])
-pcdfilename = "../output/voxeledpcd_exp1.pcd"
-o3d.io.write_point_cloud(pcdfilename, voxeledpcd)
+file = '../output/scan_exp1.pcd'
+pcd = o3d.io.read_point_cloud(file)
+voxled_pcd = pcd.voxel_down_sample(voxel_size=0.0045)
+voxeledfile = '../output/voxeledpcd_exp1.pcd'
+o3d.io.write_point_cloud(voxeledfile,voxled_pcd)
 print( f'processing time (analysis - distribution) {time.time()-start_time:.2f} second')
