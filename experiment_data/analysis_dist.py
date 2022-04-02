7663eddf0f2204560d978e45286697c9a9414457
Rachel <hson@ncsu.edu>
2022-04-01 23:59:32 -0400

    add exp. folder

--- experiment_data/analysis_dist.py
+++ experiment_data/analysis_dist.py
@@ -1,9 +1,9 @@
 import open3d as o3d
 import time 
 start_time = time.time()
-file = '../output/scan_exp1.pcd'
+file = 'exp1_woodstruct/scan_exp1.pcd'
 pcd = o3d.io.read_point_cloud(file)
 voxled_pcd = pcd.voxel_down_sample(voxel_size=0.0045)
-voxeledfile = '../output/voxeledpcd_exp1.pcd'
+voxeledfile = 'exp1_woodstruct/voxeledpcd_exp1.pcd'
 o3d.io.write_point_cloud(voxeledfile,voxled_pcd)
 print( f'processing time (analysis - distribution) {time.time()-start_time:.2f} second')
